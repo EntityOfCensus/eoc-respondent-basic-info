eda866acb97c688c4bcf1566f8ee6b0b0f4f188f
Octavian Stirbei <octavianstirbei@gmail.com>
2024-05-31 00:50:22 +0300

    clean logs

--- src/endpoints/basic-info/validate.paths.ts
+++ src/endpoints/basic-info/validate.paths.ts
@@ -1,6 +1,4 @@
 export default function validatePathId(req, res, ctx) {
-    console.log('request params id', decodeURIComponent((req.params.id + '').replace(/\+/g, '%20')));
-    console.log('req.claims.sub ', req.claims.sub );
     if(req.claims.sub != decodeURIComponent((req.params.id + '').replace(/\+/g, '%20'))) {
         return Response.json(
             {
