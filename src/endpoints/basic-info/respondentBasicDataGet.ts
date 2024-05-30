41c364a99d674442add7268748fbe616931d4fe8
Octavian Stirbei <octavianstirbei@gmail.com>
2024-05-31 01:00:21 +0300

    clean code

--- src/endpoints/basic-info/respondentBasicDataGet.ts
+++ src/endpoints/basic-info/respondentBasicDataGet.ts
@@ -6,9 +6,6 @@ import {
 import { RespondentBasicData } from "../../types";
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
-
-const decode = require('jwt-claims');
-
 export class RespondentBasicDataGet extends OpenAPIRoute {
 	static schema: OpenAPIRouteSchema = {
 		tags: ["RespondentBasicData"],
